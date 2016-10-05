b2ea0a69875263d88eb08f18f40d8319a0b27463
Mike Reich <mjreich@gmail.com>
2016-10-05 06:53:09 -0700

    Changed HasModels init to before startup

--- src/HasModels.js
+++ src/HasModels.js
@@ -23,7 +23,7 @@ export default class HasModels extends NxusModule {
       this._model_identities = identities
     })
     
-    application.on('startup', () => {
+    application.before('startup', () => {
       let mods = this.modelNames()
       if (_.isArray(mods)) {
         mods = _.object(mods, mods)
